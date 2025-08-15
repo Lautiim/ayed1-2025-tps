730a09ad28ccde90de08a9e58f37e68c9c9fea1d
Lautiim <lasalto05@gmail.com>
2025-08-15 20:58:58 -0300

    Escritura y testeo del ejercicio 1

---
+++ TP1/TP1.py
@@ -0,0 +1,42 @@
+def mayor(num1 :int, num2 :int, num3 :int) -> int:
+    """ Obtener el mayor de 3 numeros
+
+    Pre: Recibe tres numeros, los numeros deben ser enteros.
+
+    Post: Devuelve el valor del mayor de los tres numeros recibidos, en caso de que no exista un mayor unico devuelve -1
+
+    """
+    # Se verifica que los numeros sean enteros
+    assert(isinstance(num1, int)), "Los numeros deben ser enteros"
+    assert(isinstance(num2, int)), "Los numeros deben ser enteros"
+    assert(isinstance(num3, int)), "Los numeros deben ser enteros"
+
+    if num1 == num2: # Si no existe un numero mayor la funcion devuelve
+        if num1 == num3: 
+            return -1
+    else:
+        return max(num1, num2, num3)
+
+def main():
+    """ Funcion principal del programa """
+    print("--------- Bienvenido ---------")
+    
+    while True:
+        try:
+            num1 = int(input("Ingrese el primer numero.. "))
+            num2 = int(input("Ingrese el segundo numero.. "))
+            num3 = int(input("Ingrese el tercer numero.. "))
+        except ValueError:
+            print("Por favor, ingrese solo numeros enteros. Vuelva a ingresar los numeros.")
+            continue
+        
+        break
+
+    resultado = mayor(num1, num2, num3)
+    if resultado == -1:
+        print("\nNo hay un mayor unico.")
+    else:
+        print(f"\nEl mayor de los tres numeros es: {resultado}")
+
+if __name__ == "__main__":
+    main()
