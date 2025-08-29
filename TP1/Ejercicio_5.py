b34f2c32e81679ef300491f8069531a62d9b1a39
Lautiim <lasalto05@gmail.com>
2025-08-29 20:35:31 -0300

    Correccion en el Ej. 5 No habia leido la parte que dice "Ambas funciones lambda reciben como único parámetro el número a evaluar y devuelven True o False."

--- TP1/Ejercicio_5.py
+++ TP1/Ejercicio_5.py
@@ -1,26 +1,23 @@
 import random
 
 def __main__():
-    # Funcion para determinar si un numero es oblongo.
-    # Un numero es oblongo cuando es resultado de la multiplicacion de dos numeros    
-    es_oblongo = lambda i, num: i * (i+1) == num
-    es_triangular = lambda i, num: i * (i+1) / 2 == num
 
     # Generamos un numero random del 1 al 100.
-    num = random.randint(1, 100)
+    num = 10
+    
+    # Funcion para determinar si un numero es oblongo.
+    # Un numero es oblongo cuando es resultado de la multiplicacion de dos numeros    
+    es_oblongo = lambda num: any((i * (i + 1)) == num for i in range(1, num)) # Utilizando any() debido a que no se puede iterar en un lambda
+    es_triangular = lambda num: any((i * (i + 1) / 2) == num for i in range(1, num)) # any() devuelve verdadero si en algun caso se cumple la sentencia
 
     # Iteramos empezando desde 1 hasta llegar al numero generado
-    for i in range(1, num):
-        if es_oblongo(i, num): # Si se encuentra que el numero es oblongo se detiene el bucle
-            print(f"El numero {num} es oblongo")
-            break
-    else: # Si no se encuentra que el numero es oblongo el bucle finaliza y se informa al usuario.
+    if es_oblongo(num):  # Si se encuentra que el numero es oblongo se detiene el bucle
+        print(f"El numero {num} es oblongo")
+    else:  # Si no se encuentra que el numero es oblongo el bucle finaliza y se informa al usuario.
         print(f"El numero {num} no es oblongo")
 
-    for i in range(1, num):
-        if es_triangular(i, num):
-            print(f"El numero {num} es triangular")
-            break
+    if es_triangular(num):
+        print(f"El numero {num} es triangular")
     else:
         print(f"El numero {num} no es triangular")
 
