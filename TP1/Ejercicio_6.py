3d04a4f85bd77e81fb09c7094250eacfcef81ee5
Lautiim <lasalto05@gmail.com>
2025-08-29 21:10:20 -0300

    Avances con el ejercicio 6

---
+++ TP1/Ejercicio_6.py
@@ -0,0 +1,28 @@
+def concatenar_numeros(num1, num2):
+    """
+    Concatena dos números enteros positivos sin usar facilidades de Python no vistas en clase.
+
+    Args:
+        num1: El primer número entero positivo.
+        num2: El segundo número entero positivo.
+
+    Returns:
+        El número entero resultante de concatenar num1 y num2.
+    """
+
+    if num1 < 0 or num2 < 0: # Verificamos que los numeros sean enteros positivos
+        raise ValueError("Ambos números deben ser enteros positivos.")
+
+    # Calculamos el factor de escala para el numero 2 (Por ej. 567)
+    factor = 1 # En esta variable almacenamos el valor del factor que se necesita para desplazar num1 y dejar espacio a num2
+    temp = num2 # Asignamos el valor a concatenar a temp para no modificar la informacion original
+    while temp > 0: # Iteramos hasta que el valor de temp (567) sea menor a 0 (0.567)
+        factor *= 10 # En cada vuelta multiplicamos por 10 el valor del factor, ya que multiplicar por 10 nos desplaza una posicion
+        temp //= 10 # Hacemos una division entera para eliminar el digito desplazado (056.7), se eliminaria el 0 en la primer vuelta
+
+    resultado = num1 * factor + num2 # Utilizando factor desplazamos los digitos de num1 para dejar espacio a num2
+    print(f"Número 1: {num1}, Factor: {factor}, Número 2: {num2}")
+    print(f"Resultado de la concatenación: {resultado}")
+    return resultado
+
+print(concatenar_numeros(1234, 567))
