9a6df0ca3326c7bced075a23deaa8f50f77873d4
Lautiim <lasalto05@gmail.com>
2025-09-16 22:25:34 -0300

    Escritura y testeo del ejercicio 8 del TP2

--- TP2/Ejercicio_8.py
+++ TP2/Ejercicio_8.py
@@ -1,6 +1,10 @@
 def main():
     """ Funcion principal del programa """
     print("--------- Bienvenido ---------")
+    # Genaromos la lista de numeros impares entre 100 y 200, empezamos en 101 y vamos sumando de a 2
+    lista_impares = [numero for numero in range(101, 200, 2)]
+    print("Números impares entre 100 y 200:")
 
+    print(f" {lista_impares}")
 if __name__ == "__main__":
     main()
