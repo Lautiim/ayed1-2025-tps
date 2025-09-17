42602b6eab6f772930ba8bf733bb5db90d2f16e2
Lautiim <lasalto05@gmail.com>
2025-09-16 21:29:43 -0300

    Escritura y testeo del ejercicio 3 del TP2

--- TP2/Ejercicio_3.py
+++ TP2/Ejercicio_3.py
@@ -1,6 +1,30 @@
+def generar_lista_cuadrados() -> list[int]:
+    """Funcion para generar una lista con los cuadrados de los números entre 1 y un valor indicado dado por el usuario.
+
+    Pre: Ninguna precondicion.
+
+    Post: Devuelve una lista de enteros con los cuadrados desde 1 hasta el numero ingresado.
+    """
+    final = int(input("\nIngrese el valor hasta el cual desea calcular los cuadrados: "))
+    while final < 1:
+        print("El valor debe ser un entero positivo mayor a 0.")
+        final = int(input("Ingrese el valor hasta el cual desea calcular los cuadrados: "))
+    
+    lista_cuadrados = [] # Lista para almacenar los cuadrados
+
+    # Recoremos los valores desde 1 hasta el valor ingresado.
+    for valor in range(1, final + 1):
+        cuadrado = valor ** 2 # Calculamos el cuadrado
+        lista_cuadrados.append(cuadrado) # Y lo agregamos a la lista
+    return lista_cuadrados
+
 def main():
     """ Funcion principal del programa """
     print("--------- Bienvenido ---------")
+    lista_de_cuadrados = generar_lista_cuadrados()
+    
+    print("\nLos ultimos 10 valores de la lista de cuadrados son:")
+    print(lista_de_cuadrados[-10:])
 
 if __name__ == "__main__":
     main()
