ef4c2c9ac936700c49dec2f7c50401e3fa308271
Lautiim <lasalto05@gmail.com>
2025-10-29 21:08:11 -0300

    Escritura y testeo del ejerecicio 01/TP4.

---
+++ TP4/Ejercicio_01.py
@@ -0,0 +1,32 @@
+def es_capicua(cadena: str) -> bool:
+    """Funcion para saber si una cadena es capicua, sin utilizar cadenas auxiliares ni rebanadas
+
+    Pre: Recibe una cadena de caracteres
+
+    Post: Devuelve True si la cadena es capicua, False en caso contrario
+    """
+    i = 0  # índice inicial
+    j = len(cadena) - 1  # índice final
+    while i < j:
+        if cadena[i] != cadena[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+def main():
+    cadena = input("Ingrese una cadena: ")
+
+    if es_capicua(cadena):
+        print("La cadena es capicúa.")
+    else:
+        print("La cadena no es capicúa.")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+    pass
